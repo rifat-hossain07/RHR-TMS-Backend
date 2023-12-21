26ff584349bf4486201d5ede513a36095823f65b
Rifat Rabbi <rifayrabby78@gmail.com>
2023-12-21 22:32:27 +0600

    Completed Update and delete functionality

--- index.js
+++ index.js
@@ -25,15 +25,69 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     const database = client.db("RHR-TmsDB");
-    const serviceCollection = database.collection("TasksList");
+    const taskCollection = database.collection("TasksList");
 
-    // to load all data
-    app.get("/tasks", async (req, res) => {
-      const cursor = serviceCollection.find();
+    // to load all tasks
+    app.get("/tasks/:email", async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const cursor = taskCollection.find(filter);
       const result = await cursor.toArray();
       res.send(result);
     });
 
+    // to load all data
+    app.post("/addTask", async (req, res) => {
+      const task = req.body;
+      //   console.log(task);
+      const result = await taskCollection.insertOne(task);
+      //   const cursor = taskCollection.find();
+      //   const result = await cursor.toArray();
+      res.send(result);
+    });
+    // change the status of tasks
+    app.put("/tasks/:id", async (req, res) => {
+      const id = req.params.id;
+      const status = req.body.status;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          status: status,
+        },
+      };
+      const result = await taskCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+    });
+    // Update Task
+    app.put("/tasks/update/:id", async (req, res) => {
+      const id = req.params.id;
+      const title = req.body.title;
+      const description = req.body.description;
+      const deadline = req.body.deadline;
+      const priority = req.body.priority;
+      const status = req.body.status;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          title: title,
+          description: description,
+          status: status,
+          deadline: deadline,
+          priority: priority,
+        },
+      };
+      const result = await taskCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+    });
+    // delete the tasks
+    app.delete("/delete/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await taskCollection.deleteOne(filter);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log(
